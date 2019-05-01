291b9858e7b1499d7388bf495a5aa57d07f898e9
Jan Schwarz <jan.schwarz@strv.com>
2019-05-01 14:32:58 +0200

    Add SwiftLint

---
+++ ARDemoApp/AR Demo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AR Demo
+//
+//  Created by Jan on 01/05/2019.
+//  Copyright © 2019 Jan Schwarz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
